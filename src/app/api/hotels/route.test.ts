bf0e9084eda83f9c025e3f9b2fcaca4d2933d64b
Jasmine Oji <jazijaz@Jasmines-MacBook-Pro.local>
2025-01-31 11:57:22 +1100

    test: hotels api

---
+++ src/app/api/hotels/route.test.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { GET } from "./route";
+import hotels from "@/data/hotels.json";
+
+jest.mock("next/server", () => ({
+  NextResponse: {
+    json: jest.fn(),
+  },
+}));
+
+describe("Hotels API", () => {
+  it("should return the correct mock data", async () => {
+    (NextResponse.json as jest.Mock).mockReturnValueOnce({
+      status: 200,
+      json: hotels,
+    });
+
+    const response = await GET();
+
+    expect(NextResponse.json).toHaveBeenCalledWith(hotels);
+    expect(response.status).toBe(200);
+    expect(response.json).toEqual(hotels);
+  });
+});
