e49c6d2f43610ce0cc5e1bc7b382bf68218eaa5c
Jasmine Oji <jazijaz@Jasmines-MacBook-Pro.local>
2025-01-31 15:50:57 +1100

    build: set test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,21 +1,35 @@
-import type { Config } from "jest";
-import nextJest from "next/jest.js";
+import type { Config } from 'jest'
+import nextJest from 'next/jest.js'
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: "./",
-});
+  dir: './',
+})
 
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
+  coverageReporters: ['text', 'html'],
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/data/*.json',
+    '!src/types/*.ts',
+    '!src/**/components/Registery/*.{ts,tsx}',
+  ],
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+}
 
-export default createJestConfig(config);
+export default createJestConfig(config)
