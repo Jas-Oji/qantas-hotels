5e3b36ba5038ab9e38376dfe93b5a753c7c84e9e
Jasmine Oji <jazijaz@Jasmines-MacBook-Pro.local>
2025-02-01 11:48:40 +1100

    build: next image config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+    ],
+  },
 }
 
 export default nextConfig
