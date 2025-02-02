21a29108df34bc0741b24cd91beffe0d4798b2ac
Jasmine Oji <jazijaz@Jasmines-MacBook-Pro.local>
2025-02-02 18:24:03 +1100

    feat: disable default sort option

--- src/components/Sort/Sort.tsx
+++ src/components/Sort/Sort.tsx
@@ -1,22 +1,29 @@
 import { FC } from 'react'
 import { Label } from './Sort.style'
 
+import useHotels from '@/hooks/useHotels'
 import { SortEnum } from '@/types'
 
 type SortProps = {
   onChange: (sortOrder: SortEnum) => void
 }
 
-const Sort: FC<SortProps> = ({ onChange }) => (
-  <Label>
-    <p>Sort By</p>
+const Sort: FC<SortProps> = ({ onChange }) => {
+  const { sortOrder } = useHotels()
 
-    <select name="sort" onChange={(e) => onChange(e.target.value as SortEnum)}>
-      <option value={SortEnum.DEFAULT}>Select</option>
-      <option value={SortEnum.ASCENDING}>Price: Low to High</option>
-      <option value={SortEnum.DESCENDING}>Price: High to Low</option>
-    </select>
-  </Label>
-)
+  return (
+    <Label>
+      <p>Sort By</p>
+
+      <select name="sort" onChange={(e) => onChange(e.target.value as SortEnum)}>
+        <option value={SortEnum.DEFAULT} disabled={!!sortOrder}>
+          Select
+        </option>
+        <option value={SortEnum.ASCENDING}>Price: Low to High</option>
+        <option value={SortEnum.DESCENDING}>Price: High to Low</option>
+      </select>
+    </Label>
+  )
+}
 
 export default Sort
