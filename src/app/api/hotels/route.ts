3a0418993008f9c90c0e221225383bb96f556ed5
Jasmine Oji <jazijaz@Jasmines-MacBook-Pro.local>
2025-02-10 20:38:50 +1100

    refactor: api to sort data

--- src/app/api/hotels/route.ts
+++ src/app/api/hotels/route.ts
@@ -1,5 +1,16 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
+import getSortedItemsByPrice from './utils'
 
 import hotels from '@/data/hotels.json'
+import { Hotel, SortEnum } from '@/types'
 
-export const GET = async () => NextResponse.json(hotels)
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = new URL(request.url)
+  const sortBy = searchParams.get('sort') || ''
+
+  const results = getSortedItemsByPrice(hotels.results as Hotel[], sortBy as SortEnum)
+
+  return NextResponse.json({
+    results,
+  })
+}
